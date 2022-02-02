97e22dae6b3b29afdd1becc80d930b210e5319b4
Mendel Jacks <mendeljacks@gmail.com>
2022-02-01 22:40:32 -0500

    push

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as a from './extract_subpaths'
 import * as b from './deep_get'
 
-export default { ...a, ...b }
+export const extract_subpaths = a.extract_subpaths
+export const deep_get = b.deep_get
